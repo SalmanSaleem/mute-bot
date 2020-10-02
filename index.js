08353693ba8e2650cdf7a30799640298ecdee10b
Salman Saleem <salman.saleem18@gmail.com>
2020-10-02 22:10:07 +0400

    update commands

--- index.js
+++ index.js
@@ -3,27 +3,36 @@ require("dotenv").config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
+const MUTE_CMD = "!mu"
+const UNMUTE_CMD = "!un"
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
+  if (msg.content == "!help") {
+      msg.channel.send("Commands you can use:")
+      msg.channel.send(MUTE_CMD + ": Mute users")
+      msg.channel.send(UNMUTE_CMD + ": UnMute users")
+  }
+  if (msg.content == "!chotiya") {
+      msg.reply("You are chotiya!!!")
   }
 })
 
 client.on('message', (message) => {
-    if (message.content == '!mute') {
+    if (message.content == MUTE_CMD) {
         toggleMute(message, true)
      }
-    if (message.content == '!unmute') {
+    if (message.content == UNMUTE_CMD) {
         toggleMute(message, false)
     }
 });
 
 client.login(process.env.BOT_TOKEN)
 
+// helper functions
 function toggleMute(message, setMute){
     if (message.member.voice.channel) {
     let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
