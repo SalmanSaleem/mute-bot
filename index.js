7fc7b7db49095d998c1f677de9c69d2467321bb8
Salman <salman.saleem@democrance.com>
2020-10-02 16:35:53 +0400

    better replies

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ client.on("message", (msg) => {
 })
 
 client.on('message', (message) => {
-    if (message.content == '/muteAll') {
+    if (message.content == '!mute') {
         toggleMute(message, true)
      }
-    if (message.content == '/unmuteAll') {
+    if (message.content == '!unmute') {
         toggleMute(message, false)
     }
 });
@@ -25,22 +25,17 @@ client.on('message', (message) => {
 client.login(process.env.BOT_TOKEN)
 
 function toggleMute(message, setMute){
-    // Your invokation here, for example your switch/case hook for some command (i.e. '!muteall')
-    // Check if user is in a voice channel:
     if (message.member.voice.channel) {
     let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
         for (const [memberID, member] of channel.members) {
-            // I added the following if statement to mute everyone but the invoker:
-            if (setMute && member != message.member){
-                // This single line however, nested inside the for loop, should mute everyone in the channel:
+            if (setMute){
                 member.voice.setMute(true);
-                message.reply('Everyone is muted!');
             }
             else if (!setMute){
                 member.voice.setMute(false);
-                message.reply('Everyone is unmuted!');
             }
         }
+        message.reply(setMute ? 'Everyone is muted!': 'Everyone is unmuted!');
     }
     else {
         message.reply('You need to join a voice channel first!');
